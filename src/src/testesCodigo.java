92979fff84a982bae5b495bddb5f1dbf3bd594af
Bibiana Fleck de Bem <bibianadebem@gmail.com>
2022-04-14 10:48:02 -0300

    Envio da Primeira main para testes

---
+++ src/src/testesCodigo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+/**
+ *
+ * @author inaci
+ */
+public class testesCodigo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Main para testes de códigos e para aula 14/04/2022");
+        System.out.println("Bibiana de Bem");
+    }
+    
+}
